e045d91b95f0175114fffc9be0152946b50285e1
PMS22 <prathams99@rediff.com>
2019-06-06 07:52:42 +0200

    base: FireHound Gaming [1/4]
    Enabling the tile will :-
    - Show a "First-click" informative dialog
    - Change ringer mode to vibrate
    - Increase the media volume to max
    - Disable Heads Up
    - Turn off Hardware keys
    
    - Turning off the tile will change all the values back to default
    - I haven't enabled Expanded desktop as I dont think any game runs
      in non expanded mode, so better to keep it clean.
    - Duplicate the SystemSetting class from SecureSetting one grabbing
      the System Settings string :-
      https://github.com/FireHound/android_frameworks_base/blob/o8.1/packages/SystemUI/src/com/android/systemui/qs/SecureSetting.java
    
    * NEW CHANGES :- *
    
    1) User can select apps for which gaming mode will be turned on automtically.
    2) Any incoming call will be handled in the background if answered
       and no InCallUI will be shown.
    3) All notifications from the Dialer app will be shown. There won't be a full-screen
       call pick up screen on incoming calls.
    4) The tile will save the user-defined Settings state in a dummy Settings.System string
       and the same value will be restored back when tile is disabled. So now no users settings will be overrided.
    5) Added "Dynamic Gaming mode". The tile will be enabled automatically if the launched
       app is a game.
    6) Added "Lock brightness" thing. Brightness mode will be changed to manual one.
    7) Added a long press intent to open Gaming mode Settings.
    8) The "user defined" ringer state and media volume will be restored once gaming mode is turned off.
    
    Credits :-
    1) @ezio84 - storing user settings in mock Settings.
    2) @ishubhamsingh - forward port to pie and improve dynamic gaming mode
    
    Change-Id: I4d1a34be570b54e5dbd8b0099952bce1773b00e6

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -36,6 +36,7 @@ import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.FontTile;
+import com.android.systemui.qs.tiles.GamingModeTile;
 import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.HWKeysTile;
@@ -149,6 +150,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new SyncTile(mHost);
             case "font":
                 return new FontTile(mHost);
+            case "gaming":
+                return new GamingModeTile(mHost);
         }
 
         // Intent tiles.
